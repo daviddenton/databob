c60482ba1794d06b66c4023ff250cba5fd23df83
David Denton <denton.david@gmail.com>
2014-05-24 17:01:00 +0100

    added null/undefined/Nan/Infinity

--- test/converterTest.js
+++ test/converterTest.js
@@ -3,6 +3,7 @@
 var df = require('datafixture.js');
 var converter = require('../lib/converter');
 var assert = require('chai').assert;
+var _ = require('lodash');
 
 describe('Converter', function () {
 
@@ -12,17 +13,35 @@ describe('Converter', function () {
         });
     }
 
+    function generatedValueForInputOf(value) {
+        return df.generate({
+            value: converter(value)
+        }).value;
+    }
+
     itConverts('string with no spaces to a word', 'qwewe', 'lorem:1');
     itConverts('string with spaces to sentence with maximum words', 'qwewe qweqwe', 'lorem:1...2');
     itConverts('string with new lines to multi-line', 'qwewe qweqwe\nasd\nasdsad asdas', 'plorem:3');
     itConverts('empty string to possibly empty string', '', 'lorem:0...1');
     itConverts('GUID', 'b7d9f73b-3d9d-286c-561a-e11ffdc0a484', 'GUID');
+
     itConverts('positive number to positive integer as a maximum', 20, '1...20');
     itConverts('negative number to negative integer as a minimum', -20, '-20...0');
     itConverts('positive decimal to decimal with rounded positive integer as a maximum', 19.23, '1...20:2');
     itConverts('positive exact decimal to decimal with rounded positive integer as a maximum', 19.0, '1...19');
     itConverts('negative decimal to decimal with rounded negative integer as a maximum', -19.24, '-20...0:2');
     itConverts('negative exact decimal to decimal with rounded negative integer as a maximum', -19.0, '-19...0');
+
+    it('supports +Infinity', function () {
+        assert.equal(generatedValueForInputOf(Infinity), Infinity);
+    });
+    it('supports -Infinity', function () {
+        assert.equal(generatedValueForInputOf(-Infinity), -Infinity);
+    });
+    it('supports NaN', function () {
+        assert.ok(_.isNaN(generatedValueForInputOf(NaN)));
+    });
+
     itConverts('empty object', {}, {});
     itConverts('simple object', {
         aProperty: 'qwewe'
@@ -40,16 +59,21 @@ describe('Converter', function () {
     });
 
     it('converts boolean', function () {
-        assert.equal(typeof df.generate({
-            value: converter(true)
-        }).value, 'boolean');
+        assert.ok(typeof generatedValueForInputOf(true), 'boolean');
+    });
+
+    it('converts undefined', function () {
+        assert.equal(generatedValueForInputOf(undefined), undefined);
+    });
+
+    it('converts null', function () {
+        assert.equal(generatedValueForInputOf(null), null);
     });
 
     //console.log(    df.generate(converter(['asd asd', 'sdf sdf'])()));
     //    itConverts('boolean', true, 'boolean');
 
     // ALSO: dates and time formats
-    // ALSO: undefined, null, NaN, infinity,
     // ALSO: REGISTER CUSTOM CONVERTERS
     // ALSO - OBJECTS!
     // 1. named primitives as properties
